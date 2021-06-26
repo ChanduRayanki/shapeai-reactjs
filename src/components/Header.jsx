f525875a315c5b2fd1ef4f9771deb078b467954b
ChanduRayanki <63344309+ChanduRayanki@users.noreply.github.com>
2021-06-26 21:39:56 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h2>SHAPE AI</h2>
+      <h1> Bootcamps</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1> JavaScript and React.js</h1>
+      <p> A basic bootcamp on Javascript and React.js</p>
+    </div>
+  );
+}
+
+export default Info;
